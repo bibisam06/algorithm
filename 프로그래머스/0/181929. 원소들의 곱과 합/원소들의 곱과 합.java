d3c106d32f1ee79f08414d9a7c604b1f97060ff0
한비 <hb.jade00@gmail.com>
2025-08-31 22:14:12 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,39 +1,17 @@
 class Solution {
     public int solution(int[] num_list) {
-        int answer = 0;
-        
-        
-        int sum = getSum(num_list);
-        int sum2 = getTime(num_list);
-        
-        
-        if(sum2 < Math.pow(sum, 2)){
-            answer = 1;
-        }else{
-            answer = 0;
-        }
-        
-        return answer;
-    }
-    
-    
-    public int getSum(int[] num_list){
         int sum = 0;
+        int product = 1;
         
-        for(int i=0; i<num_list.length; i++){
+        for(int i = 0; i < num_list.length; i++){
             sum += num_list[i];
+            product *= num_list[i];
         }
         
-        return sum;
-    }
-    
-    public int getTime(int[] num_list){
-        int sum = 1;
-        for(int i=0; i<num_list.length; i++){
-            sum *= num_list[i];
+        if(sum * sum > product){
+            return 1;
+        } else {
+            return 0;
         }
-        
-        return sum;
-        
     }
 }
