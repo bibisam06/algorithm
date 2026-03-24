fed5d6bdc522fdaa8866da87d8b82156decae5e3
한비 <hb.jade00@gmail.com>
2026-03-24 21:10:24 +0900

    [Silver IV] Title: 수 찾기, Time: 1204 ms, Memory: 178656 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,23 +1,52 @@
 import java.util.*;
-
+/*
+1920번 : 수 찾기(실버4)
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
 
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < N; i++) {
-            set.add(sc.nextInt());
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
         }
+        Arrays.sort(nums);
+        int m = sc.nextInt();
+        int[] nums2 = new int[m];
+        for (int i = 0; i < m; i++) {
+            nums2[i] = sc.nextInt();
+        }
+        //입력받기 끄읕
 
-        int M = sc.nextInt();
-        for (int j = 0; j < M; j++) {
-            int value = sc.nextInt();
-            if (set.contains(value)) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
+        for(int i : nums2){
+            if(BinarySearch(nums, i, 0, nums.length-1)){
+                sb.append(1).append("\n");
+            }else{
+                sb.append(0).append("\n");
             }
         }
+
+        System.out.println(sb);
+
+    }
+
+    public static boolean BinarySearch(int[] nums, int target, int start, int end){
+
+
+        if(start> end){
+            return false;
+        }
+
+        int mid = (start+end)/2;
+        if(nums[mid]<target){  //오른쪾 반쪾으로가서탐색
+            return BinarySearch(nums, target, mid+1, end);
+        }else if(nums[mid] == target){ //nums[mid] > target -> 왼쪾반쪽으로가서탐색
+            return true;
+        }else{
+            return BinarySearch(nums,target, start, mid-1);
+        }
+
     }
 }
