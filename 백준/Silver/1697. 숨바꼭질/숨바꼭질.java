01dd29d9ec054e00b248de7f73c3943cd7a282af
한비 <hb.jade00@gmail.com>
2026-03-28 01:10:43 +0900

    [Silver I] Title: 숨바꼭질, Time: 224 ms, Memory: 23952 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,37 +1,40 @@
+//package bfs.silver.Num1697;
+
 import java.util.*;
-
+/*
+1697번 : 숨바꼭질(실버1)
+ */
 public class Main {
-    static final int MAX = 100000;
-
+    static int[] dist = new int[100001];//10만좌표까지가능함
+    static boolean[] visited = new boolean[100001];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int k = sc.nextInt();
 
+        bfs(n, k);
+    }
 
-        if (n >= k) {
-            System.out.println(n - k);
-            return;
-        }
-
-        int[] dist = new int[MAX + 1];
-        Arrays.fill(dist, -1);
-
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(n);
+    static void bfs(int n, int k){
+        Deque<Integer> queue = new ArrayDeque<>();
         dist[n] = 0;
+        queue.offer(n);
+        visited[n] = true;
 
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            if (cur == k) {
+        while(!queue.isEmpty()){
+            int cur = queue.poll();
+            if(cur==k){
                 System.out.println(dist[cur]);
                 return;
             }
-            int[] nexts = {cur - 1, cur + 1, cur * 2};
-            for (int nx : nexts) {
-                if (0 <= nx && nx <= MAX && dist[nx] == -1) {
-                    dist[nx] = dist[cur] + 1;
-                    q.offer(nx);
+            int[] can = {cur+1, cur-1, cur*2};
+
+            for(int i : can){
+                if(i >= 0 && i <= 100000 && !visited[i]){
+                    dist[i] = dist[cur] + 1;
+                    queue.offer(i);
+                    visited[i] = true;
                 }
             }
         }
