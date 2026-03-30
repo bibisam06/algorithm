150102d0d2662be87c606b837fa6f1d91ecdaa37
한비 <hb.jade00@gmail.com>
2026-03-30 21:28:45 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,39 +1,53 @@
 import java.util.*;
-
+import java.io.*;
+/*
+2606번 : 바이러스 (
+ */
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt(); // 총 컴퓨터 수
-        List<List<Integer>> virusList = new ArrayList<>();
 
-        for (int i = 0; i < a + 1; i++) {
-            virusList.add(new ArrayList<>());
+    static int answer = 0;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<>());
         }
 
-        int b = sc.nextInt(); // 연결 쌍 수
-        for (int i = 0; i < b; i++) {
-            int i1 = sc.nextInt();
-            int i2 = sc.nextInt();
-            virusList.get(i1).add(i2);
-            virusList.get(i2).add(i1);
+        for(int i=0; i<m; i++){
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(stk.nextToken());
+            int b = Integer.parseInt(stk.nextToken());
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
+        //스타트는 항상 1로 고정이 되어있음
+        //System.out.println(graph);
+        answer = bfs(graph, 1);
 
-        Queue<Integer> queue = new ArrayDeque<>();
+        System.out.println(answer-1);
+    }
+
+    public static int bfs(List<List<Integer>> graph, int start){
         Set<Integer> visited = new HashSet<>();
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(start);
+        visited.add(start);
 
-        queue.add(1);
-        visited.add(1); // 시작 노드 방문 처리 필수!
-
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            for (int next : virusList.get(current)) {
-                if (!visited.contains(next)) {
-                    queue.add(next);
+        while(!queue.isEmpty()){
+            int current = queue.remove();
+            for(int next : graph.get(current)){
+                if(!visited.contains(next)){
                     visited.add(next);
+                    queue.add(next);
                 }
             }
         }
 
-        System.out.println(visited.size() - 1); // 1번 제외
+        return visited.size();
     }
 }
