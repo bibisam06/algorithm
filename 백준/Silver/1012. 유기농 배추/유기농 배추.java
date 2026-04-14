27d9bb95a6898314a62d770f2dabc21db4d7db63
한비 <hb.jade00@gmail.com>
2026-04-14 10:34:22 +0900

    [Silver II] Title: 유기농 배추, Time: 328 ms, Memory: 32116 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,64 +2,62 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //테스트 케이스 구현 나중에
         Scanner sc = new Scanner(System.in);
 
-        int T = sc.nextInt();
-        for (int q = 0; q < T; q++) {
-            int m = sc.nextInt();
-            int n = sc.nextInt();
-            int x = sc.nextInt();
+        int t = sc.nextInt();
+        for(int j=0; j<t; j++){
+            int m = sc.nextInt(); // 가로
+            int n = sc.nextInt(); // 세로
+            int k = sc.nextInt();
 
-            int[][] cabbage = new int[m][n];
+            boolean[][] visited = new boolean[n][m];
+            int[][] map = new int[n][m];
+            int[][] cabbage = new int[k][2];
+            int answer = 0;
 
-
-            Queue<int[]> queue = new ArrayDeque<>();
-            boolean[][] visited = new boolean[m][n];
-
-            for(int i=0; i<x; i++){
-                int t1 = sc.nextInt();
-                int t2 = sc.nextInt();
-                cabbage[t1][t2] = 1;
+            for (int i = 0; i < k; i++) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                map[y][x] = 1;
+                cabbage[i][0] = y; // row
+                cabbage[i][1] = x; // col
             }
 
+            for (int i = 0; i < k; i++) {
+                int y = cabbage[i][0];
+                int x = cabbage[i][1];
 
-            int counter = 0;
-            int[] directX = {1, -1, 0, 0};
-            int[] directY = {0, 0, 1, -1};
-
-            for(int i=0; i<m; i++){
-                for(int j=0; j<n; j++){
-                    if(!visited[i][j] && cabbage[i][j] == 1){
-                        visited[i][j] = true;
-                        queue.offer(new int[]{i, j});
-
-                        counter++;
-
-                        while (!queue.isEmpty()) {
-                            int[] p = queue.poll();
-                            int cx = p[0], cy = p[1];
-
-                            for (int dir = 0; dir < 4; dir++) {
-                                int nx = cx + directX[dir];
-                                int ny = cy + directY[dir];
-
-                                if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
-                                    // 👉 추가된 조건: 배추가 있어야 하고, 아직 방문 안 한 칸만 큐에 넣음
-                                    if (cabbage[nx][ny] == 1 && !visited[nx][ny]) {
-                                        visited[nx][ny] = true;
-                                        queue.offer(new int[]{nx, ny});
-                                    }
-                                }
-                            }
-                        }
-
-                    }
+                if (!visited[y][x]) {
+                    bfs(map, visited, y, x, n, m);
+                    answer++;
                 }
             }
 
+            System.out.println(answer);
+        }
+    }
 
-            System.out.println(counter);
+    static void bfs(int[][] map, boolean[][] visited, int y, int x, int n, int m) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{y, x});
+        visited[y][x] = true;
+
+        int[] dy = {-1, 1, 0, 0};
+        int[] dx = {0, 0, -1, 1};
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int ny = cur[0] + dy[i];
+                int nx = cur[1] + dx[i];
+
+                if (ny >= 0 && ny < n && nx >= 0 && nx < m
+                        && map[ny][nx] == 1 && !visited[ny][nx]) {
+                    visited[ny][nx] = true;
+                    queue.offer(new int[]{ny, nx});
+                }
+            }
         }
     }
 }
