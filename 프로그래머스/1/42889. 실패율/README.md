56bb3a8c99dc8febbb7beefc2fa4c72be6d3cf25
한비 <hb.jade00@gmail.com>
2026-07-19 20:56:31 +0900

    [level 1] Title: 실패율, Time: 31.01 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
 
 ### 성능 요약
 
-메모리: 93.5 MB, 시간: 404.26 ms
+메모리: 89.7 MB, 시간: 31.01 ms
 
 ### 구분
 
@@ -16,13 +16,13 @@
 
 ### 제출 일자
 
-2025년 10월 10일 22:13:17
+2026년 07월 19일 20:56:28
 
 ### 문제 설명
 
 <h2>실패율</h2>
 
-<p><img src="https://grepp-programmers.s3.amazonaws.com/files/production/bde471d8ac/48ddf1cc-c4ea-499d-b431-9727ee799191.png" title="" alt="failture_rate1.png"></p>
+<p><img src="https://asset.programmers.co.kr/files/production/bde471d8ac/48ddf1cc-c4ea-499d-b431-9727ee799191.png" title="" alt="failture_rate1.png"></p>
 
 <p>슈퍼 게임 개발자 오렐리는 큰 고민에 빠졌다. 그녀가 만든 프랜즈 오천성이 대성공을 거뒀지만, 요즘 신규 사용자의 수가 급감한 것이다. 원인은 신규 사용자와 기존 사용자 사이에 스테이지 차이가 너무 큰 것이 문제였다.</p>
 
--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -3,30 +3,54 @@ import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
         int[] answer = new int[N];
-        List<double[]> failures = new ArrayList<>(); // {스테이지 번호, 실패율}
+        int n = stages.length;
 
-        for (int i = 1; i <= N; i++) {
-            int reach = 0;  // i단계 도달한 사람
-            int fail = 0;   // i단계에서 멈춘 사람
+        int[] arr1 = new int[N];   // 도달한 사람 수
+        int[] arr2 = new int[N];   // 실패한 사람 수
+        double[] arr3 = new double[N];
 
-            for (int s : stages) {
-                if (s >= i) reach++;  
-                if (s == i) fail++;   
+        for (int i = 0; i < n; i++) {
+            int number = stages[i];
+
+         
+            if (number != N + 1) {
+                arr2[number - 1]++;
             }
 
-            double rate = (reach == 0) ? 0 : (double) fail / reach;
-            failures.add(new double[]{i, rate});
+            
+            if (number == N + 1) {
+              
+                for (int j = 0; j < N; j++) {
+                    arr1[j]++;
+                }
+            } else {
+                
+                for (int j = 0; j < number; j++) {
+                    arr1[j]++;
+                }
+            }
         }
-
-
-        failures.sort(Comparator.comparing((double[] a) -> a[1])
-                                .reversed()
-                                .thenComparing(a -> a[0]));
 
         for (int i = 0; i < N; i++) {
-            answer[i] = (int) failures.get(i)[0];
+            if (arr1[i] == 0)
+                arr3[i] = 0;
+            else
+                arr3[i] = (double) arr2[i] / arr1[i];   
         }
 
+        Map<Integer, Double> map = new HashMap<>();
+        
+        for(int i=0; i<N; i++){
+            map.put(i+1, arr3[i]); 
+        }
+        
+        List<Integer> list = new ArrayList<>(map.keySet());
+        
+       list.sort((a, b) -> Double.compare(map.get(b), map.get(a)));
+        
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
         return answer;
     }
 }
